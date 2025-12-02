2f5552e885c3fca563b06385f8fb2df5f6b65b82
luismolinaboni22-art <luismolinaboni22@gmail.com>
2025-12-02 14:24:18 -0600

    Update models.py

---
+++ k9_app/models.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class Visitante(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    cedula = db.Column(db.String(50), nullable=False)
+    empresa = db.Column(db.String(100), nullable=True)
+    persona_visita = db.Column(db.String(100), nullable=False)
+    motivo = db.Column(db.String(200), nullable=True)
+    placa = db.Column(db.String(20), nullable=True)
+    hora_ingreso = db.Column(db.DateTime, default=datetime.utcnow)
+    hora_salida = db.Column(db.DateTime, nullable=True)
+
+    def __repr__(self):
+        return f"<Visitante {self.nombre}>"
